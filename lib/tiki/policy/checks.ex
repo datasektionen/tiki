d74197edf378704aa5f015f0d2346021f22c9f37
Adrian Salamon <adr.salamon@gmail.com>
2025-10-30 17:17:42 +0100

    fix: scope based checks

--- lib/tiki/policy/checks.ex
+++ lib/tiki/policy/checks.ex
@@ -5,15 +5,15 @@ defmodule Tiki.Policy.Checks do
 
   @permission_service Application.compile_env(:tiki, :permission_service_module)
 
-  def hive(%Accounts.Scope{user: user}, _object, permission), do: hive(user, nil, permission)
+  def hive(%Accounts.Scope{user: user}, object, permission), do: hive(user, object, permission)
 
   def hive(%Accounts.User{} = user, _object, permission) do
     @permission_service.get_permissions(user)
     |> Enum.any?(fn p -> p == permission end)
   end
 
-  def any_team_role(%Accounts.Scope{user: user}, _object, role),
-    do: any_team_role(user, nil, role)
+  def any_team_role(%Accounts.Scope{user: user}, object, role),
+    do: any_team_role(user, object, role)
 
   def any_team_role(%Accounts.User{id: id}, _object, role) do
     case Teams.get_memberships_for_user(id) do
@@ -22,7 +22,7 @@ defmodule Tiki.Policy.Checks do
     end
   end
 
-  def team_role(%Accounts.Scope{user: user}, _object, role), do: team_role(user, nil, role)
+  def team_role(%Accounts.Scope{user: user}, object, role), do: team_role(user, object, role)
 
   def team_role(%Accounts.User{id: id}, %Teams.Team{id: team_id}, role) do
     case Teams.get_members_for_team(team_id) do
