308350feac16f032d0bf64be1e1b22fc0fde81a9
Viktor Ekby <viktoe@datasektionen.se>
2026-08-20 11:11:25 +0200

    fix stripe env

--- config/runtime.exs
+++ config/runtime.exs
@@ -87,6 +87,11 @@ if config_env() == :prod do
   stripe_webhook_secret =
     System.get_env("STRIPE_WEBHOOK_SECRET") || raise "STRIPE_WEBHOOK_SECRET is not set"
 
+  config :tiki,
+    stripe_api_key: stripe_api_key,
+    stripe_public_key: stripe_public_key,
+    stripe_webhook_secret: stripe_webhook_secret
+
   config :tiki, TikiWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     http: [
@@ -97,10 +102,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base,
-    stripe_api_key: stripe_api_key,
-    stripe_public_key: stripe_public_key,
-    stripe_webhook_secret: stripe_webhook_secret
+    secret_key_base: secret_key_base
 
   # Oidc login
   oidc_issuer_url = System.get_env("OIDC_ISSUER_URL") || "https://sso.datasektionen.se/op"
